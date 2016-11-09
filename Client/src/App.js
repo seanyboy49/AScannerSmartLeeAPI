4bf31da3b2b3c317ab09e280d0252c586a1aa1a9
Sean Lee <seanboramlee@gmail.com>
2016-11-08 17:35:32 -0800

    add styling to shoe cards

--- Client/src/App.js
+++ Client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import $ from 'jquery'
 import './App.css';
 
+
 var App = React.createClass({
   getInitialState: function() {
     return { data: [] }
@@ -22,16 +23,20 @@ var App = React.createClass({
 
   render: function() {
     return(
-      <div>
+      <div className="Top-Selling-Items">
         {this.state.data.map(function(shoe) {
           return(
-            <div>
-             <p>{shoe.brand}</p>
-             <p>{shoe.style}</p>
-             <p>{shoe.quantity}</p>
-             <p>{shoe.size}</p>
-             <p>{shoe.UPC}</p>
-             <p>{shoe.description}</p>
+            <div className="Shoe-Card-Container" key={shoe.id}>
+              <div className="Image">
+                <img src= "Classic-Short.jpg" width="100%" height="auto"/>
+              </div>
+            <div className="Shoe-Stats">
+               <h1>{shoe.brand} {shoe.style}</h1>
+               <p>Quantity: {shoe.quantity}</p>
+               <p>Size: {shoe.size}</p>
+               <p>UPC Code: {shoe.UPC}</p>
+               <p>Description: {shoe.description}</p>
+             </div>
            </div>
           )
         })}
@@ -39,42 +44,5 @@ var App = React.createClass({
     )
   }
 })
-
-
-// class App extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       data: [],
-//     }
-//     this.componentWillMount = this.componentWillMount.bind(this);
-//   };
-//
-//   componentWillMount = () => {
-//     console.log("Inside allShoes")
-//      fetch(`/api/shoe`)
-//      .then(
-//        function(response) {
-//          if (response.status !== 200) {
-//            console.log('Looks like there was a problem');
-//            return;
-//          }
-//          response.json().then(function(data) {
-//            this.setState({data: data})
-//          }).bind(this);
-//        }
-//      )
-//   }
-//
-//
-//
-//   render () {
-//     return(
-//     <div>
-//         {this.state.data}
-//     </div>
-//     )
-//   }
-// }
 
 export default App;
