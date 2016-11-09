500e9ad22e08f6227c4c0e15b0507f1623330845
Frank Aiello <frankvaiello@gmail.com>
2016-11-09 12:37:17 -0800

    Refactor of jquery ajax call to fetch call

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import $ from 'jquery'
 import './App.css';
 import Inventory from './Inventory'
 
@@ -10,22 +9,22 @@ var App = React.createClass({
   },
 
   componentWillMount: function() {
-    $.ajax({
-      url: `/api/shoe`,
-      dataType: "json",
-      success: function(data) {
-        this.setState({data: data})
-      }.bind(this),
-      error: function(xhr, status, err) {
-        console.error(this, status, err.toString())
-      }.bind(this)
-    })
+    return fetch('/api/shoe')
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data)
+        this.setState({
+          data: data
+        });
+      })
+      .catch((error) => {
+        console.log(error)
+      });
   },
 
   render: function() {
     return(
       <div className="Top-Selling-Items">
-        // {console.log(this.state.data, 'this.state.data')}
         <Inventory items={this.state.data} />
       </div>
     )
