a960a7b6b5432fc526e4591f430e46746f5cbf3d
Sean Lee <seanboramlee@gmail.com>
2016-11-09 17:58:04 -0800

    create new associations

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,101 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Brand.create!([
+  {name: "Ugg"},
+  {name: "Nike"},
+  {name: "SAS"}
+  ])
+
+Style.create!([
+  {style: "W Bailey Button II", style_number: 1016226, brand: "Ugg"},
+  {style: "W Classic Short II", style_number: 1016223, brand: "Ugg"},
+  {style: "W Bailey Bow II", style_number: 1016225, brand: "Ugg"}
+  ])
 
 Shoe.create!([
-  {brand: "UGG", style: "W Bailey Button II", style_number: "1016226", color: "BLK", quantity: 3, size: 7, UPC: 190108110654, description:"Keep feet cute and cozy in these Bailey Button II boots from UGG, which feature a sheepskin exposed shaft that can stay straight up or be rolled down, and a signature logo etched wood button with an elastic hook-and-loop band strap.", created_at: DateTime.new(2001,2,3,4,5,6), updated_at: DateTime.new(2017,6, 5, 4, 3, 2, 1)},
-  {brand: "UGG", style: "W Bailey Button II", style_number: "1016226", color: "BLK", quantity: 5, size: 8, UPC: 190108110661, description:"Keep feet cute and cozy in these Bailey Button II boots from UGG, which feature a sheepskin exposed shaft that can stay straight up or be rolled down, and a signature logo etched wood button with an elastic hook-and-loop band strap.", created_at: DateTime.new(2001,2,3,4,5,6), updated_at: DateTime.new(2017,6, 5, 4, 3, 2, 1)},
-  {brand: "UGG", style: "W Classic Short II", style_number: "1016223", color: "CHE", quantity: 12, size: 6, UPC: 190108095494, description:"Keep feet cute and cozy in these Classic Short boots from UGG, which feature plush sheekskin construction both inside and out, and a shaft that can stay straight up or be rolled down. Featured in chestnut", created_at: DateTime.new(2001,2,3,4,5,6), updated_at: DateTime.new(2017,6, 5, 4, 3, 2, 1)},
-  {brand: "UGG", style: "W Classic Short II", style_number: "1016223", color: "CHE", quantity: 23, size: 8, UPC: 190108095517, description:"Keep feet cute and cozy in these Classic Short boots from UGG, which feature plush sheekskin construction both inside and out, and a shaft that can stay straight up or be rolled down. Featured in chestnut", created_at: DateTime.new(2001,2,3,4,5,6), updated_at: DateTime.new(2017,6, 5, 4, 3, 2, 1)},
-  {brand: "UGG", style: "W Bailey Bow II", style_number: "1016225", color: "GREY", quantity: 15, size: 8, UPC: 190108090338, description:"Two bows adorably update our iconic boot which boasts soft Twinface that has been pretreated for protection against water and stains. Also new is our revolutionary Treadlite by UGG sole which cushions feet and grips exceptionally well on both wet and dry surfaces.", created_at: DateTime.new(2001,2,3,4,5,6), updated_at: DateTime.new(2017,6, 5, 4, 3, 2, 1)}
-])
+  {color: "BLK", size: 5, quantity: 4, upc: 190108110650, style: "W Bailey Button II"},
+  {color: "BLK", size: 5.5, quantity: 1, upc: 190108110651, style: "W Bailey Button II"},
+  {color: "BLK", size: 6, quantity: 3, upc: 190108110652, style: "W Bailey Button II"},
+  {color: "BLK", size: 6.5, quantity: 12, upc: 190108110653, style: "W Bailey Button II"},
+  {color: "BLK", size: 7, quantity: 24, upc: 190108110654, style: "W Bailey Button II"},
+  {color: "BLK", size: 7.5, quantity: 9, upc: 190108110655, style: "W Bailey Button II"},
+  {color: "BLK", size: 8, quantity: 41, upc: 190108110656, style: "W Bailey Button II"},
+  {color: "BLK", size: 8.5, quantity: 12, upc: 1901081157, style: "W Bailey Button II"},
+  {color: "BLK", size: 9, quantity: 9, upc: 190108110658, style: "W Bailey Button II"},
+  {color: "BLK", size: 10, quantity: 1, upc: 190108110659, style: "W Bailey Button II"},
+  {color: "BLK", size: 11, quantity: 0, upc: 190108110660, style: "W Bailey Button II"},
+  {color: "BLK", size: 12, quantity: 1, upc: 190108110661, style: "W Bailey Button II"}
+  ])
 
-Shoe.where(style: "W Bailey Button II")
+Shoe.create!([
+  {color: "CHE", size: 5, quantity: 4, upc: 190108110640, style: "W Bailey Button II"},
+  {color: "CHE", size: 5.5, quantity: 1, upc: 190108110641, style: "W Bailey Button II"},
+  {color: "CHE", size: 6, quantity: 3, upc: 190108110642, style: "W Bailey Button II"},
+  {color: "CHE", size: 6.5, quantity: 12, upc: 190108110643, style: "W Bailey Button II"},
+  {color: "CHE", size: 7, quantity: 24, upc: 190108110644, style: "W Bailey Button II"},
+  {color: "CHE", size: 7.5, quantity: 9, upc: 190108110645, style: "W Bailey Button II"},
+  {color: "CHE", size: 8, quantity: 41, upc: 190108110646, style: "W Bailey Button II"},
+  {color: "CHE", size: 8.5, quantity: 12, upc: 190108110647, style: "W Bailey Button II"},
+  {color: "CHE", size: 9, quantity: 9, upc: 190108110648, style: "W Bailey Button II"},
+  {color: "CHE", size: 10, quantity: 1, upc: 190108110649, style: "W Bailey Button II"},
+  {color: "CHE", size: 11, quantity: 0, upc: 190108110650, style: "W Bailey Button II"},
+  {color: "CHE", size: 12, quantity: 1, upc: 190108110651, style: "W Bailey Button II"}
+  ])
+
+Shoe.create!([
+  {color: "CHE", size: 5, quantity: 4, upc: 190108095490, style: "W Classic Short II"},
+  {color: "CHE", size: 5.5, quantity: 1, upc: 190108095491, style: "W Classic Short II"},
+  {color: "CHE", size: 6, quantity: 3, upc: 190108095492, style: "W Classic Short II"},
+  {color: "CHE", size: 6.5, quantity: 12, upc: 190108095493, style: "W Classic Short II"},
+  {color: "CHE", size: 7, quantity: 24, upc: 190108095494, style: "W Classic Short II"},
+  {color: "CHE", size: 7.5, quantity: 9, upc: 190108095495, style: "W Classic Short II"},
+  {color: "CHE", size: 8, quantity: 41, upc: 190108095496, style: "W Classic Short II"},
+  {color: "CHE", size: 8.5, quantity: 12, upc: 190108095497, style: "W Classic Short II"},
+  {color: "CHE", size: 9, quantity: 9, upc: 190108095498, style: "W Classic Short II"},
+  {color: "CHE", size: 10, quantity: 1, upc: 190108095499, style: "W Classic Short II"},
+  {color: "CHE", size: 11, quantity: 0, upc: 190108095500, style: "W Classic Short II"},
+  {color: "CHE", size: 12, quantity: 1, upc: 190108095501, style: "W Classic Short II"}
+  ])
+
+Shoe.create!([
+  {color: "GREY", size: 5, quantity: 4, upc: 190108095480, style: "W Classic Short II"},
+  {color: "GREY", size: 5.5, quantity: 1, upc: 190108095481, style: "W Classic Short II"},
+  {color: "GREY", size: 6, quantity: 3, upc: 190108095482, style: "W Classic Short II"},
+  {color: "GREY", size: 6.5, quantity: 12, upc: 190108095483, style: "W Classic Short II"},
+  {color: "GREY", size: 7, quantity: 24, upc: 190108095484, style: "W Classic Short II"},
+  {color: "GREY", size: 7.5, quantity: 9, upc: 190108095485, style: "W Classic Short II"},
+  {color: "GREY", size: 8, quantity: 41, upc: 190108095486, style: "W Classic Short II"},
+  {color: "GREY", size: 8.5, quantity: 12, upc: 190108095487, style: "W Classic Short II"},
+  {color: "GREY", size: 9, quantity: 9, upc: 190108095488, style: "W Classic Short II"},
+  {color: "GREY", size: 10, quantity: 1, upc: 190108095489, style: "W Classic Short II"},
+  {color: "GREY", size: 11, quantity: 0, upc: 190108025490, style: "W Classic Short II"},
+  {color: "GREY", size: 12, quantity: 1, upc: 190108025491, style: "W Classic Short II"}
+  ])
+
+Shoe.create!([
+  {color: "GREY", size: 5, quantity: 4, upc: 190108090330, style: "W Bailey Bow II"},
+  {color: "GREY", size: 5.5, quantity: 1, upc: 190108090331, style: "W Bailey Bow II"},
+  {color: "GREY", size: 6, quantity: 3, upc: 190108090332, style: "W Bailey Bow II"},
+  {color: "GREY", size: 6.5, quantity: 12, upc: 190108090333, style: "W Bailey Bow II"},
+  {color: "GREY", size: 7, quantity: 24, upc: 190108090334, style: "W Bailey Bow II"},
+  {color: "GREY", size: 7.5, quantity: 9, upc: 190108090335, style: "W Bailey Bow II"},
+  {color: "GREY", size: 8, quantity: 41, upc: 190108090336, style: "W Bailey Bow II"},
+  {color: "GREY", size: 8.5, quantity: 12, upc: 190108090337, style: "W Bailey Bow II"},
+  {color: "GREY", size: 9, quantity: 9, upc: 190108090338, style: "W Bailey Bow II"},
+  {color: "GREY", size: 10, quantity: 1, upc: 190108090339, style: "W Bailey Bow II"},
+  {color: "GREY", size: 11, quantity: 0, upc: 190108090340, style: "W Bailey Bow II"},
+  {color: "GREY", size: 12, quantity: 1, upc: 190108090341, style: "W Bailey Bow II"}
+  ])
+
+Shoe.create!([
+  {color: "CHE", size: 5, quantity: 4, upc: 190108090340, style: "W Bailey Bow II"},
+  {color: "CHE", size: 5.5, quantity: 1, upc: 190108090341, style: "W Bailey Bow II"},
+  {color: "CHE", size: 6, quantity: 3, upc: 190108090342, style: "W Bailey Bow II"},
+  {color: "CHE", size: 6.5, quantity: 12, upc: 190108090343, style: "W Bailey Bow II"},
+  {color: "CHE", size: 7, quantity: 24, upc: 190108090344, style: "W Bailey Bow II"},
+  {color: "CHE", size: 7.5, quantity: 9, upc: 190108090345, style: "W Bailey Bow II"},
+  {color: "CHE", size: 8, quantity: 41, upc: 190108090346, style: "W Bailey Bow II"},
+  {color: "CHE", size: 8.5, quantity: 12, upc: 190108090347, style: "W Bailey Bow II"},
+  {color: "CHE", size: 9, quantity: 9, upc: 190108090348, style: "W Bailey Bow II"},
+  {color: "CHE", size: 10, quantity: 1, upc: 190108090349, style: "W Bailey Bow II"},
+  {color: "CHE", size: 11, quantity: 0, upc: 190108090350, style: "W Bailey Bow II"},
+  {color: "CHE", size: 12, quantity: 1, upc: 190108090351, style: "W Bailey Bow II"}
+  ])
